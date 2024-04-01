3572d8e88b31fe4b14bc257867084770306b2f23
Suraj Moon <surajmoon@Mindstixs-MacBook-Pro.local>
2024-04-01 10:29:24 +0530

    null

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        //System.out.println("Hello, World!");
-        System.out.println("this is to be opened");
         System.out.println("not openedasddasd ");
     }
 }
